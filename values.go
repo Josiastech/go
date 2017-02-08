609d9320ddc102432c2362b49de08ffe08990545
Josias Chocooj <jchocooj@katch.com>
2017-02-08 12:18:41 -0600

    Update GO Learning examples

---
+++ values.go
@@ -0,0 +1,23 @@
+/**
+ * Go has various value types including strings, integers, floats, booleans, etc.
+ */
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// Strings, which can be added together with +.
+	fmt.Println("Go" + "lang")
+
+	// Integers and floats.
+	fmt.Println("1+1 =", 1+1)
+
+	fmt.Println("7.0/3.0 =", 7.0/3.0)
+
+	// Booleans, with boolean operators as you’d expect.
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
