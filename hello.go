db0b11eb1177b1f50c47cda7d50e70e04c5cd75e
Josias <josiastech@gmail.com>
2017-02-16 17:54:39 -0600

    UPDATE: add local files

---
+++ hello.go
@@ -0,0 +1,12 @@
+package main
+
+// FMT package provides the basic formatting
+// function and exposes ever C-like experience for reading,
+// writing and formatting strings.
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello World!")
+}
