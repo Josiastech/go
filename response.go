fbcf6c1bfd16c332fa1ef96e1a547df88eddb4d0
Josias <josiastech@gmail.com>
2017-02-17 11:08:02 -0600

    Sending Headers Only

---
+++ response.go
@@ -0,0 +1,14 @@
+package main
+
+import "net/http"
+
+func main(){
+	http.HandleFunc("/", foo)
+	http.ListenAndServe(":3000", nil)
+}
+
+
+func foo(w http.ResponseWriter, req *http.Request){
+	w.Header().Set("Server", "A GO web Server")
+	w.WriteHeader(200)
+}
